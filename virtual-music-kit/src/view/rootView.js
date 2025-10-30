b2aacdbb53f44930e3ca58e46f8e6949cac34e1d
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2025-10-30 17:56:24 +0100

    feat: add view rendering

--- virtual-music-kit/src/view/rootView.js
+++ virtual-music-kit/src/view/rootView.js
@@ -1,4 +1,6 @@
 import ElementCreator from "../../utils/element-creator.js";
+import XylophoneView from "./XylophoneView.js";
+import HeaderView from "./headerView.js";
 export default class RootView extends ElementCreator {
   constructor() {
     super({
@@ -9,5 +11,9 @@ export default class RootView extends ElementCreator {
   createView() {
     const main = this.getElement();
     document.body.append(main);
+    const xylophoneView = new XylophoneView();
+    const headerView = new HeaderView();
+    main.append(headerView.createView());
+    main.append(xylophoneView.createView());
   }
 }
--- virtual-music-kit/src/view/xylophoneView.js
+++ virtual-music-kit/src/view/xylophoneView.js
@@ -1,2 +1,36 @@
 import ElementCreator from "../../utils/element-creator.js";
-export default class XylophoneView extends ElementCreator {}
+import { NOTES } from "../model/data/keys.js";
+export default class XylophoneView extends ElementCreator {
+    constructor(){
+        super({
+            tag: "div",
+            classNames: ["xylophone"]
+        });
+        this.onNoteClickHandler = null;
+    }
+    createView(){
+        const xylophone = this.getElement();
+        this.createKeys(xylophone);
+        return xylophone;
+    }
+    onKeyClick(handler) { this.onNoteClickHandler = handler; }
+    createKeys(xylophone){
+        NOTES.forEach(note=>{
+            const keyBox =new ElementCreator({ tag:"div", classNames: [`${note.id}-key-box`, "key-box"]});
+            const key = new ElementCreator({ tag:"div", classNames: [`${note.id}`, "key"], 
+                textContent: `${note.label}`, callback: () => {this.onKeyClickHandler?.(`${note.id}`)}});
+            const keyLabel = new ElementCreator({ tag:"div", classNames: [`${note.id}-label`, "key-label"], textContent: `${note.keyLabel}`});
+            const keyLabelBox = new ElementCreator({ tag:"div", classNames: ["key-label-box"]});
+            const editKeyLabel = new ElementCreator({ tag:"div", classNames: [`${note.id}-edit-label`, "edit-key-label"]});
+            const editIcon = new ElementCreator({ tag:"img", classNames: ["edit-img"],attributesNames: [["src", "/assets/editIcon.png"]]});
+
+            editKeyLabel.addInnerElement(editIcon);
+            keyLabelBox.addInnerElement(keyLabel);
+            keyLabelBox.addInnerElement(editKeyLabel);
+            keyBox.addInnerElement(key);
+            keyBox.addInnerElement(keyLabelBox)
+            xylophone.append(keyBox.getElement())
+        })
+
+    }
+}
